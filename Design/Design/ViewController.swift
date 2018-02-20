780324599efb5f43ca13cdd2cada48b96c71e61f
Charlesjv <35268266+Charlesjv@users.noreply.github.com>
2018-02-20 12:53:35 -0500

    Add files via upload

---
+++ Design/Design/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  Design
+//
+//  Created by MacStudent on 2018-02-20.
+//  Copyright © 2018 CJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+   
+    
+    @IBOutlet weak var txtEmail: UITextField!
+    
+    @IBOutlet var lblEmail: UIView!
+    
+    @IBAction func btnLogin(_ sender: Any) {
+    }
+    /*set alert
+     
+ */
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
