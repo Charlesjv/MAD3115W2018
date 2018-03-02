dfaf29b5d41fc731046c579b0c194741dae0937c
Charlesjv <35268266+Charlesjv@users.noreply.github.com>
2018-03-02 12:03:14 -0500

    Add files via upload

---
+++ day9/day9/Bookscell.swift
@@ -0,0 +1,24 @@
+//
+//  Bookscell.swift
+//  day9
+//
+//  Created by MacStudent on 2018-03-02.
+//  Copyright © 2018 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class Bookscell: UICollectionViewCell {
+  
+    
+    
+    @IBOutlet weak var imgbook: UIImageView!
+    
+    
+    @IBOutlet weak var lblbooktitle: UILabel!
+ 
+    
+    
+    
+    
+}
